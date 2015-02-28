8dafe41ae9815563af2ddd4a7868d948302c9e38
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-02-28 18:34:36 +0000

    refactored

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,18 +1,15 @@
 class Cell
 
-  attr_accessor :value, :candidates
+  attr_accessor :value, :candidates, :neighbours
 
   def initialize(value = 0)
     @value = value
     @candidates = (1..9).to_a
+    @neighbours = []
   end
 
   def has_value?
     value != 0
-  end
-
-  def neighbours
-    @neighbours = []
   end
 
 end
